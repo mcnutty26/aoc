863277bb9a4159549ad49fc8c69d5dc737ac8292
mcnutty <william.seymour@kellogg.ox.ac.uk>
2018-12-19 23:12:01 +0000

    think elfcc is done?

--- elfcc/elfcc.py
+++ elfcc/elfcc.py
@@ -9,7 +9,11 @@ program = []
 with open(sys.argv[1]) as f:
     for line in f: program.append(line.strip('\n').split(' '))
 
-ip_r = int(program.pop(0)[1])
+if program[0][0] == "#ip":
+    ip_r = int(program.pop(0)[1])
+else:
+    ip_r = 6
+
 start = """
 #include <stdio.h>
 
@@ -22,13 +26,13 @@ int bani(int a, int b){return a & b;}
 int borr(int a, int b){return a | b;}
 int bori(int a, int b){return a | b;}
 int setr(int a, int b){return a;}
-int seti(int a, int b){return b;}
+int seti(int a, int b){return a;}
 int gtir(int a, int b){return a > b;}
 int gtri(int a, int b){return a > b;}
 int gtrr(int a, int b){return a > b;}
-int eqir(int a, int b){return a == b;}
-int eqri(int a, int b){return a == b;}
-int eqrr(int a, int b){return a == b;}
+int eqir(int a, int b){return a == b;
+int eqri(int a, int b){return a == b;
+int eqrr(int a, int b){return a == b;
 
 int main()
 {
@@ -40,27 +44,32 @@ int main()
     };
     
     int ip = 0;
-    int r[6] = {0,0,0,0,0,0};
+    int r[7] = {0,0,0,0,0,0,0};
     int (*opcodes[16])(int x, int y) = {addr,addi,mulr,muli,banr,bani,borr,bori,setr,seti,gtir,gtri,gtrr,eqir,eqri,eqrr};
-    
 """
 
 end = """
-    printf("%d, %d, %d, %d, %d, %d\\n",r[0], r[1], r[2], r[3], r[4], r[5]);
+    printf("[%d, %d, %d, %d, %d, %d]\\n",r[0], r[1], r[2], r[3], r[4], r[5]);
     return 0;
 }
 """
 
 loop = f"""
-    for (int i = 0; i < len; i++) {{
-        int a = ops[i].a;
-        int b = ops[i].b;
-        int c = ops[i].c;
+    do {{
         r[{ip_r}] = ip;
-        r[c] = (*opcodes[codes[ip]])(a, b);
+        if (codes[ip]==0||codes[ip]==2||codes[ip]==4||codes[ip]==6||codes[ip]==8||codes[ip]==12||codes[ip]==15){{
+            r[ops[ip].c] = (*opcodes[codes[ip]])(r[ops[ip].a], r[ops[ip].b]);
+        }}
+        else if (codes[ip]==1||codes[ip]==3||codes[ip]==5||codes[ip]==7||codes[ip]==11||codes[ip]==14){{
+            r[ops[ip].c] = (*opcodes[codes[ip]])(r[ops[ip].a], ops[ip].b);
+        }}
+        else {{
+            r[ops[ip].c] = (*opcodes[codes[ip]])(ops[ip].a, r[ops[ip].b]);
+        }}
         ip = r[{ip_r}];
         ip++;
-    }}"""
+//        printf("%d, %d, %d, %d, %d, %d\\n",r[0], r[1], r[2], r[3], r[4], r[5]);
+    }} while (ip > 0 && ip < len);"""
 
 ops = ["addr", "addi", "mulr", "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtir", "gtri", "gtrr", "eqir", "eqri", "eqrr"]
 
