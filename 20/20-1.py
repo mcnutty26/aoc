d87c457affdeb140fca595a8a4a11d2d25fabf2a
mcnutty <william.seymour@kellogg.ox.ac.uk>
2018-12-21 01:52:15 +0000

    minimised day 20

--- 20/20-1.py
+++ 20/20-1.py
@@ -2,32 +2,22 @@ def parse(raw):
     global longest_path
     children = []
     path = []
-
     while len(raw):
         next_char = raw.pop(0)
-
-        if next_char == '(':
-            path.append(parse(raw))
-
+        if next_char == '(': path.append(parse(raw))
         elif next_char == ')':
             children.append(path)
             return children
-
         elif next_char == '|':
             children.append(path)
             path = []
-
-        else:
-            path.append(next_char)
-
+        else: path.append(next_char)
     return path
-
 def extract(path):
     p = ""
     for item in path:
         if type(item) == type('A'):
             p += item
-            print
         else:
             m = ""
             for sub_item in item:
@@ -36,7 +26,6 @@ def extract(path):
                     m = result
             p += m
     return p
-
 def reduced(path):
     while path.count('NS') or path.count('EW') or path.count('SN') or path.count('WE'):
         path = path.replace("NS","")
@@ -44,18 +33,9 @@ def reduced(path):
         path = path.replace("EW","")
         path = path.replace("WE","")
     return path
-
 raw = ""
-with open("20.txt") as f:
-    raw = f.readline().strip('\n')
-
-#get rid of ^ and $ characters
+with open("20.txt") as f: raw = f.readline().strip('\n')
 raw = list(raw[1:-1])
-
-#parse the tree
 longest_path = ""
 t = parse(raw)
-
-#find the longest path
-c = extract(t)
-print(len(reduced(c)))
+print(len(reduced(extract(t))))
--- 20/20-2.py
+++ 20/20-2.py
@@ -5,28 +5,19 @@ d = dict({'N':[0,1], 'E': [1,0], 'S': [0,-1], 'W': [-1,0]})
 processed = []
 raw = ""
 points = set()
-
 def explore(regex):
     global loc
-
     while len(regex):
         c = regex.pop(0)
-        if c == '(':
-            stack.append(loc)
-
-        elif c == ')':
-            stack.pop()
-
-        elif c == '|':
-            loc = stack[-1]
-
+        if c == '(': stack.append(loc)
+        elif c == ')': stack.pop()
+        elif c == '|': loc = stack[-1]
         else:
             nloc = (loc[0] + d[c][0], loc[1] + d[c][1])
             if nloc not in comesfrom:
                 comesfrom[nloc] = loc
                 points.add(nloc)
             loc = nloc
-
 def chart(l):
     i = 0
     x = l[0]
@@ -35,12 +26,8 @@ def chart(l):
         i += 1
         x,y = comesfrom[(x,y)]
     return i
-
-with open("20.txt") as f:
-    raw = f.readline().strip('\n')
-for char in raw:
-    processed.append(char)
-
+with open("20.txt") as f: raw = f.readline().strip('\n')
+for char in raw: processed.append(char)
 counter = 0
 explore(processed[1:-1])
 print(max(chart(x) for x in points))
