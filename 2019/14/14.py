f3c062feef1bb1e01f11ea0a8c36eaec3d241af0
mcnutty <william.seymour@kellogg.ox.ac.uk>
2019-12-20 22:17:16 +0000

    went back and optimised day 14

--- 2019/14/14.py
+++ 2019/14/14.py
@@ -1,10 +1,8 @@
+from math import ceil
 from collections import defaultdict
 
 productions = {}
-produced = defaultdict(int)
-allocated = defaultdict(int)
-quantities = {}
-guess = 1
+produced = {}
 GOAL = 1000000000000
 
 with open('input.txt', 'rt') as textfile:
@@ -17,31 +15,54 @@ with open('input.txt', 'rt') as textfile:
             temp = item.strip().split(' ')
             reactants.append((temp[1], int(temp[0])))
         productions[product[1]] = reactants
-        quantities[product[1]] = int(product[0])
+        produced[product[1]] = int(product[0])
 
-def make(compound, quantity):
-    # We can always find some more ORE
-    if compound == "ORE":
-        produced["ORE"] += quantity
-        return
-    
-    # We have all of the ingredients on hand
-    elif produced[compound] - allocated[compound] >= quantity:
-        return
-    
-    #We need to manufacture the ingredients
+def make(compound: str, quantity: int):
+    ore = 0
+    stack = [(compound, quantity)]
+    extra = defaultdict(int)
+
+    while len(stack) > 0:
+        a, b = stack.pop(0)
+        current = [a, b]
+
+        if current[0] == "ORE":
+            ore += current[1]
+            continue
+           
+        # if we have some extra left over, use it now
+        if extra[current[0]] >= current[1]:
+            extra[current[0]] -= current[1]
+            continue
+        elif extra[current[0]] > 0:
+            current[1] -= extra[current[0]]
+            extra[current[0]] = 0
+
+        # we need to do the reaction this many batches
+        total = ceil(current[1] / produced[current[0]])
+        
+        # maybe we made too much
+        extra[current[0]] = max(0, (produced[current[0]] * total) - current[1])
+
+        # order the ingredients
+        for req in productions[current[0]]:
+            stack.append((req[0], total * req[1]))
+
+    return ore
+
+print(make("FUEL", 1))
+
+low = 1
+high = GOAL
+guess = None
+
+while low <= high:
+    guess = ceil((low + high) / 2)
+    n = make("FUEL", guess)
+    if n > GOAL:
+        high = guess - 1
     else:
-        while produced[compound] - allocated[compound] < quantity:
-            for req in productions[compound]:
-                make(req[0], req[1])
-                allocated[req[0]] += req[1]
-            produced[compound] += quantities[compound]
-        return
+        low = guess + 1
 
-make("FUEL", 1)
-print(produced["ORE"])
+print(guess)
 
-while produced["ORE"] < GOAL:
-    guess += 1
-    make("FUEL", guess)
-print(guess - 1)
